0e1ad56774318f7a5e6af73214fcaf7a74dba96b
schough <schough@ucsd.edu>
2016-11-03 23:22:12 -0700

    Update reverse_endian.c

--- reverse_endian.c
+++ reverse_endian.c
@@ -1,7 +1,13 @@
+/*
+ * Sooyeon E. Chough (A92093139)
+ * Yue Jiang (A92095681)
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "warmup.h"
 
-int main(int argc, char** argv){
+int main(int argc, char** argv)
+{
      /* Your main code for reversing endian-ness goes here*/
 }
