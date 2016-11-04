c5daad1cd4cfbfb1fe706ede8fe1b44fec7e3a9f
schough <schough@ucsd.edu>
2016-11-03 23:21:08 -0700

    Update warmup.c

--- warmup.c
+++ warmup.c
@@ -1,12 +1,19 @@
+/*
+ * Sooyeon E. Chough (A92093139)
+ * Yue Jiang (A92095681)
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "warmup.h"
 
-unsigned int *char_freqs(FILE *fp){
+unsigned int *char_freqs(FILE *fp)
+{
    /* Your code goes here*/
 }
   
 
-int reorder_bytes(int num){
+int reorder_bytes(int num)
+{
    /* Your code goes here*/
 }
