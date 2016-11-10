9ed28caf5ec79497dae4647b39be566ff2ff4a73
yuej <yuj062@ucsd.edu>
2016-11-09 23:52:28 -0800

    Update warmup.c

--- warmup.c
+++ warmup.c
@@ -1,8 +1,8 @@
-/*
- * Sooyeon E. Chough (A92093139)
- * Yue Jiang (A92095681)
- */
 
+/* Sooyeon E. Chough (A92093139)
+ * Yue Jiang (A92095681)
+ *
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include "warmup.h"
@@ -14,30 +14,35 @@ unsigned int *char_freqs(FILE *fp)
    {
       return NULL;
    }
-   // Making array and other necessary variables
-   unsigned int *ascii[ 256 ];
-   int c;
+   // Making array
+   unsigned int* ascii = calloc(256,sizeof(int));
+
    // Getting first char to start loop
-   char ch = fgetc( fp );
-   // Looping through the file 
-   while( ch != EOF && ch != '\n' )
+   char ch = “”;
+   int c = 0;
+   // Looping through the file
+   while(1)
    {      
+      if(feof(fp)){
+        break;
+      }
+      ch = fgetc(fp);
       // Converting to int
-      c = (int) ch;
+      c = (int) ch; 
       // Incrementing array
       ascii[ c ] = ascii[ c ] + 1;
-      // Getting char 
-      ch = fgetc( fp );
    }
+   // Closing the stream
+   fclose( fp ); 
    // Returning array
-   return *ascii;
-}
-  
-
+   return ascii;
+}   
+ 
+    
 int reorder_bytes(int num)
-{
+{   
    // Creating necessary int
-   int reverse = 0;
+   int reverse = 0; 
    int mask = 0xff;
    // Getting each necessary bit, reordering, and adding
    reverse = reverse + ( ( num & mask ) << 8 );
