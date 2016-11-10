8581c3aaf8bf5ad8a148cb6e14ce65c8a7f532f0
yuej <yuj062@ucsd.edu>
2016-11-09 23:50:54 -0800

    Update reverse_endian.c

--- reverse_endian.c
+++ reverse_endian.c
@@ -1,5 +1,5 @@
-/*
- * Sooyeon E. Chough (A92093139)
+
+/*Sooyeon E. Chough (A92093139)
  * Yue Jiang (A92095681)
  *
  * For testing warmup methods
@@ -13,29 +13,35 @@ int main(int argc, char** argv)
 {
      // Initializing variables for later usage
      FILE *fi, *fo;
-     int *ptr;
-     int flip;
-     
+     int *ptr = malloc(sizeof(int));
+     ptr = NULL;
+     int *flip = malloc(sizeof(int));
+     flip = NULL;
+
      // Accounting for if not enough arguments when opening file
      if( argc < 3 )
      {
           return 0;
      }
-     // Opening file 
+     // Opening file
      fi = fopen( argv[ 1 ], "r" );
      fo = fopen( argv[ 2 ], "w" );
      // Getting first number
-     ptr = fread( ptr, 4, 1, fi );
      // Going through whole file
-     while( ptr == sizeof( int ) )
-     {
+     while(1){
           // Flipping bytes and writing to output file
-          flip = reorder_bytes( ptr );
-          ptr = fwrite( flip, 4, 1, fo );
+          fread(ptr,4,1,fi);
+          *flip = reorder_bytes( *ptr );
+          fwrite((const void*)flip, 4, 1, fo );
+          if(feof(fo)){
+            break;
+          }
      }
      // Closing the files
      fclose( fo );
      fclose( fi );
+     free(ptr);
+     free(flip);
      // Ending main
      return 0;
 }
