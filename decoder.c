132cb62bfa26b7c8b695b90dfb38b1ff643912b8
schough <schough@ucsd.edu>
2016-11-10 16:19:25 -0800

    Update decoder.c

--- decoder.c
+++ decoder.c
@@ -85,29 +85,29 @@ void codeToBinary(FILE *in, FILE *out, int index)
 {
     // Initializing necessary variables 
     char ch;
-    char *code = NULL;
-    int bit;
+    //char *code = NULL;
+    int *bit;
     // Reading file
     ch = fgetc( in );
     // Looping through whole file
     while( ch != EOF )
     {
         // Getting bit and char verison aka '0' or '1'
-        bit = extractBit( ch, index );
+        *bit = extractBit( ch, index );
         // Allocating memory
-        code = (char*) malloc( sizeof(char) );
-        /*if( bit == 0 )
+        //code = (char*) malloc( sizeof(char) );
+        if( *bit == 0 )
         {
-            *code = '0';
+            *bit == (int) '0';
         }
         else
         {
-            *code = '1';
-        }*/
+            *bit == (int) '1';
+        }
         // Writing to file 
-        fwrite( bit, sizeof(char), 1, out );
+        fwrite( bit, sizeof(int), 1, out );
         // Freeing memory
-        free( code );
+        //free( code );
         // Getting next char
         ch = fgetc( in );
     }
