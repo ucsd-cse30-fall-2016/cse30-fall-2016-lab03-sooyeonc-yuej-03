14a28cfb43cb1e2bd575a72b99fce0e3726169b7
yuej <yuj062@ucsd.edu>
2016-11-18 15:39:31 -0800

    Update binaryToText.s

--- binaryToText.s
+++ binaryToText.s
@@ -29,10 +29,10 @@ binaryToText:
 while:
    CMP r6, #-1 @check EOF
    BLE end
-   MOV r0, #24 @malloc needs 4x6
+   MOV r0, #6 @malloc needs 4x6
    BL malloc
-   MOV r7, r0 @r7 stores bin
-   MOV r8, r0
+   MOV r7, r0 @r7 stores bin's address
+   MOV r8, #0
    
    CMP r8, #5
    BLE loop
@@ -42,10 +42,11 @@ back:
    MOV r0, r7
    BL decodeChar
    
-   MOV r7, r0 @move bin back to r7
+   STR r0, [r7]
+   MOV r7, r0
    MOV r1, #1
    MOV r2, #1
-   MOV r3, r5
+   MOV r3, r6
    BL fwrite
    
    MOV r0, r7
@@ -56,6 +57,7 @@ loop: STR r6, [r7,r8]
    MOV r0, r4
    BL fgetc
    MOV r6, r0
+   ADD r8, r8, #1
    CMP r8, #5
    BLE loop
    B back
