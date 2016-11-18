90474ae6ff0196ef14311d30c8c737a0bbb4f0fd
schough <schough@ucsd.edu>
2016-11-18 15:31:39 -0800

    Update decodeFile.s

--- decodeFile.s
+++ decodeFile.s
@@ -1,3 +1,6 @@
+@ Sooyeon E. Chough (A92093139)
+@ Yue Jiang (A92095681)
+
 .syntax unified
 
 .text
@@ -15,8 +18,17 @@ decodeFile:
     @ YOUR CODE GOES HERE
     @-----------------------
     
-    @ (your code)
-    @ put your return value in r0 here:
+    @ r5 is input
+    MOV r5, r0
+    @ r6 is bin
+    MOV r6, r1
+    @ r7 is output
+    MOV r7, r2
+    @ r8 is index
+    MOV r8, r3
+    @ r1 is 'r' aka read
+    MOV r1, #114
+    BL fopen
 
     @-----------------------
 return:
