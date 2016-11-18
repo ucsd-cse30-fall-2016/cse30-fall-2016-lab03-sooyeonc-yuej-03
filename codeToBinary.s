f832ecbf5e06ee7c901501b58221609a4332c00e
schough <schough@ucsd.edu>
2016-11-18 14:42:53 -0800

    Update codeToBinary.s

--- codeToBinary.s
+++ codeToBinary.s
@@ -18,34 +18,34 @@ codeToBinary:
 
     @-----------------------
     
-    @ r4 is copy of input file
-    MOV r4, r0
-    @ r5 is copy of output file
-    MOV r5, r1
-    @ r6 is index
-    MOV r6, r2
-    @ r7 holds bit
-    MOV r7, #0
-    @ holds char
+    @ r5 is copy of input file
+    MOV r5, r0
+    @ r6 is copy of output file
+    MOV r6, r1
+    @ r7 is index
+    MOV r7, r2
+    @ r8 holds bit
     MOV r8, #0
+    @ r9 holds char
+    MOV r9, #0
     @ r0 is pointer
-    MOV r0, #4
+    MOV r0, #1
     BL malloc
-    @ move pointer to r9
-    MOV r9, r0
+    @ move address of pointer to r4
+    MOV r4, r0
 loop: 
-    @ get input file once more
-    MOV r0, r4
+    @ get input file (once more)
+    MOV r0, r5
     BL fgetc
-    @ mov char to r8
-    MOV r8, r0
+    @ mov char to r4
+    MOV r4, r0
     @ check EOF
-    CMP r8, #0
+    CMP r4, #0
     BEQ end
     @ move char to r0
-    MOV r0, r8
+    MOV r0, r4
     @ index at r1
-    MOV r1, r6
+    MOV r1, r7
     BL extractBit
     @ compare bit to '0'
     CMP r0, #48
@@ -55,10 +55,10 @@ loop:
     B skipAssignZero
 assignZero:
     @ assign 0
-    MOV r9, #48
+    MOV r4, #48
 skipAssignZero:
-    @ move pointer to r9
-    MOV r0, r9
+    @ move pointer to r0
+    MOV r0, r4
     @ moving other necessary values for fwrite
     MOV r1, #1
     MOV r2, #1
@@ -68,7 +68,7 @@ skipAssignZero:
     B loop
     
 end:
-    MOV r0, r9
+    MOV r0, r4
     BL free
 
     @-----------------------
