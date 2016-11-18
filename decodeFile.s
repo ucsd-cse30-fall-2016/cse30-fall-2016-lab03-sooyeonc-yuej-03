df18f0a6c34bf0ed6675feaa8654c2697224ab70
schough <schough@ucsd.edu>
2016-11-18 15:39:36 -0800

    Update decodeFile.s

--- decodeFile.s
+++ decodeFile.s
@@ -18,15 +18,54 @@ decodeFile:
     @ YOUR CODE GOES HERE
     @-----------------------
     
-    @ r5 is input
-    MOV r5, r0
-    @ r6 is bin
-    MOV r6, r1
-    @ r7 is output
-    MOV r7, r2
-    @ r8 is index
-    MOV r8, r3
+    @ r4 is bin
+    MOV r4, r1
+    @ r5 is output
+    MOV r5, r2
+    @ r6 is index
+    MOV r6, r3
+    
+    @ open input file
     @ r1 is 'r' aka read
+    MOV r1, #114
+    BL fopen
+    
+    @ input file is at r7
+    MOV r7, r0
+    
+    @ open bin file
+    @ r0 is bin char array 
+    MOV r0, r5
+    @ r1 is 'w' aka write
+    MOV r1, #119
+    BL fopen
+    
+    @ bin file is at r8
+    MOV r8, r0
+    
+    @ codeToBinary( input, bin, index )
+    @ input is r0
+    MOV r0, r5
+    @ bin is r1
+    MOV r1, r8
+    @ index is r2
+    MOV r2, r6
+    BL codeToBinary
+    
+    @ bin file is r8
+    MOV r8, r0
+    
+    @ fclose input file
+    BL fclose
+    
+    @ fclose bin file
+    MOV r0, r8
+    BL fclose
+    
+    @ open bin file 
+    @ bin char array is r0
+    MOV r0, r4
+    @ 'r' is r1
     MOV r1, #114
     BL fopen
 
